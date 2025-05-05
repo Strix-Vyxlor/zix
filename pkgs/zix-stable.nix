b940961e87ef709d9c4924718074c627b571c427
Strix Vyxlor <strix.vyxlor@gmail.com>
2025-05-05 14:50:50 +0200

    update hash

--- pkgs/zix-stable.nix
+++ pkgs/zix-stable.nix
@@ -5,7 +5,7 @@
 }: let
   src = fetchzip {
     url = "https://github.com/Strix-Vyxlor/zix/archive/refs/tags/0.3.4.tar.gz";
-    hash = "sha256-0cwDHs4x92YoEF/Lpj18AiVwD0M+V3yENcjgXxdCmAM=";
+    hash = "sha256-1WdmjCdgN7yK4vwUYVvOaZMkI5+pKC8i/c7UDx8INP4=";
   };
 in
   env.package {
